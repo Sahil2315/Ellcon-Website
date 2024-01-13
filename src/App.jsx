ab86577117a8cb0bfac9d33b93f633bda5617426
Sahil2315 <153sahilnigam@gmail.com>
2024-01-13 09:40:11 +0530

    fade applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from 'react'
 import EllconLogo from './assets/Logo.png'
-import LPBD from './assets/LPBD.jpg'
+import marker from './assets/marker.png'
+import logotext from './assets/logotext.png'
+import LPBD from './assets/LPBD.png'
 import Navbar from './Navbar'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  function removeCover() {
+    const Cover = document.getElementById('cover')
+    setTimeout('', 2000);
+    Cover.style.display = 'none'
+  }
   return (
     <>
     <img className='LPBD' src={LPBD}/>
+      <div id="cover">
+        <img id='marker' src={marker}/>
+        <img id='logotext' src={logotext}/>
+      </div>
       <div className='maindiv'>
         <div className="headline">
           <div className="headbg"></div>
           <img className='logoimg' src={EllconLogo}/>
-
           <Navbar/>
         </div>
       </div>
