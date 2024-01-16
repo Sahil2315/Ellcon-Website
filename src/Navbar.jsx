dd9ee5ffabab957ac827edffa7bb09c433a577f5
Sahil2315 <153sahilnigam@gmail.com>
2024-01-16 18:38:57 +0530

    Responsive Navbar 3

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,28 +10,25 @@ function Navbar(){
         let line1 = document.getElementById("nbline1")
         let line2 = document.getElementById("nbline2")
         let line3 = document.getElementById("nbline3")
-        
+        let navE = document.querySelectorAll('.navElements')
         if(toggle){
             line2.style.opacity = "100%"
             toggle = false
-            navbar.style.display = "none"
-            navbar.classList.remove('navHeightVert')
+            navbar.style.height = "0px"
             line1.style.transform = "rotate(0deg) scaleX(1)"
             line3.style.transform = "rotate(0deg) scaleX(1)"
-
         }
         else{
             line2.style.opacity = "0%"
             toggle = true
-            navbar.style.display = "flex"
-            navbar.classList.add('navHeightVert')
+            navbar.style.height = "330px"
             line1.style.transform = "rotate(45deg) scaleX(1.21)"
             line3.style.transform = "rotate(-45deg) scaleX(1.21)"
         }        
     }
     return(
         <div>
-            <div id="navbar" className="navbar">
+            <div id="navbar">
                 <NavLink className={ ({isActive}) => isActive ? activeLinkStyling : normalLinkStyling } to="/">Home</NavLink>
                 <NavLink className={ ({isActive}) => isActive ? activeLinkStyling : normalLinkStyling } to="/services">Services</NavLink>
                 <NavLink className={ ({isActive}) => isActive ? activeLinkStyling : normalLinkStyling } to="/projects">Our Projects</NavLink>
